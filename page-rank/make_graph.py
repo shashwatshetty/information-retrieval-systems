225ba6ec53fa13ec34482f4063f31a17e379ffe7
sha5hw4t <shashwat_shetty@hotmail.com>
2018-03-04 21:39:34 -0500

    added comments to make_graph.py, added logic for links with zero in-links

--- page-rank/make_graph.py
+++ page-rank/make_graph.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+# global variables
 bfs_crawled_links = []
 dfs_crawled_links = []
 bfs_in_links = {}
@@ -9,37 +10,60 @@ bfs_out_links = {}
 dfs_in_links = {}
 dfs_out_links = {}
 
+'''
+    Given: a file name and list to store the unique crawled links
+    Effect: populates the given list with the links extracted from the given file name.
+'''
 def get_crawled_links(file_name, crawled_links):
-    with open(file_name, 'r') as file:
+    with open(file_name, 'r') as file:                                  # open file
         line = file.readlines()
-    crawled_links.extend([l.strip() for l in line])
+    crawled_links.extend([l.strip() for l in line])                     # strip line of line break and add to list
 
+
+'''
+    Given: a url, list of crawled links and 2 dictionaries to store in-links and out-links
+    Effect: populates the in-link dictionary with the given url as value for it's corresponding out-links as keys
+                and the out-link dictionary with url as key & corresponding out-links as values in a list.
+'''
 def get_inlinks_outlinks(url, crawled_links, in_links, out_links):
-    wiki_prefix = 'https://en.wikipedia.org'
-    url_href = str(url).split('/wiki/')[-1]
-    out_links[url_href] = []
+    wiki_prefix = 'https://en.wikipedia.org'                                            # wikipedia main link
+    url_href = str(url).split('/wiki/')[-1]                                             # wikipedia document link
     page = requests.get(url).text
-    soup = BeautifulSoup(page, 'html.parser').find('div', {'id': 'mw-content-text'})
-    anchor = soup.find_all('a', {'href': re.compile("^/wiki")})
+    soup = BeautifulSoup(page, 'html.parser').find('div', {'id': 'mw-content-text'})    # getting all content text only
+    anchor = soup.find_all('a', {'href': re.compile("^/wiki")})                         # getting only wiki links
     for a in anchor:
         link = a.get('href')
         url_filter = ':' not in str(link) and '#' not in str(link) and 'Main_Page' not in str(link)
-        href = str(link).split('/wiki/')[-1]
+        href = str(link).split('/wiki/')[-1]               # child document link
         if url_filter:
             link = wiki_prefix + link
-            if link in crawled_links:
+            if link in crawled_links:                      # only add child link if crawled
                 out_links[url_href].append(href)
-                if href in in_links:
+                if href in in_links:                       # avoid duplicate in-links
                     if url_href not in in_links[href]:
                         in_links[href].append(url_href)
-                else:
-                    in_links[href] = [url_href]
+                # else:
+                #     in_links[href] = [url_href]
 
 
+'''
+    Given: list of crawled links and 2 dictionaries to store in-links and out-links
+    Effect: populates the in-link and out-link dictionaries for every url in the crawled link list
+'''
 def get_links(crawled_links, in_links, out_links):
-    for l in crawled_links:
+    for link in crawled_links:                                          # initialise in-link & out-link dictionary keys
+        link = str(link).split('/wiki/')[-1]
+        in_links[link] = []
+        out_links[link] = []
+    for l in crawled_links:                                             # get in-links & out-links for every link
         get_inlinks_outlinks(l, crawled_links, in_links, out_links)
 
+
+'''
+    Given: a dictionary and a file name
+    Effect: writes all the key mapped to the list of links on one line
+                in a .txt file with the given file name.
+'''
 def write_graph(graph_links, file_name):
     with open(file_name, 'w') as outfile:
         for key in graph_links:
@@ -47,6 +71,7 @@ def write_graph(graph_links, file_name):
             for link in graph_links[key]:
                 outfile.write("\t%s" %link)
             outfile.write("\n")
+
 
 # main method
 def main():
@@ -59,6 +84,7 @@ def main():
     get_links(dfs_crawled_links, dfs_in_links, dfs_out_links)
     write_graph(dfs_in_links, 'dfs_inlinks_graph.txt')
     write_graph(dfs_out_links, 'dfs_outlinks_graph.txt')
+
 
 if __name__ == '__main__':
     main()
