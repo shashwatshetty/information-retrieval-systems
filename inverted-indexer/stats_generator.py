18dc32f8eb55accbfffca462727153dad633fc33
sha5hw4t <shashwat_shetty@hotmail.com>
2018-03-23 18:55:02 -0400

    added comments to stats generator code

--- inverted-indexer/stats_generator.py
+++ inverted-indexer/stats_generator.py
@@ -1,53 +1,83 @@
 import json
 import operator
 
+'''
+    Given: a filename
+    Returns: a dictionary of the data structure stored in the json file
+'''
 def read_from_json(file_name):
     reader = open(file_name, 'r')
     dict = json.load(reader)
     reader.close()
     return dict
 
+'''
+    Given: an inverted index
+    Returns: a dictionary of the term to term frequency mapping
+'''
 def create_tf_table(inv_index):
-    tf_table = {}
+    tf_table = {}                               # term frequency table
     for term in inv_index:
-        count = sum(inv_index[term].values())
-        tf_table[term] = count
+        count = sum(inv_index[term].values())   # count of that particular term across all doc IDs
+        tf_table[term] = count                  # map term to count
     return tf_table
 
+
+'''
+    Given: an inverted index
+    Returns: a dictionary of the term to documents frequency mapping
+'''
 def create_df_table(inv_index):
-    df_table = {}
+    df_table = {}                               # document frequency table
     for term in inv_index:
-        docs = list(inv_index[term].keys())
-        df_table[term] = docs
+        docs = list(inv_index[term].keys())     # stores the list of all doc IDs the term appears in
+        df_table[term] = docs                   # map term to list of doc IDs
     return df_table
 
+
+'''
+    Given: a term frequency table, document frequency table an integer n and a file name
+    Effect: writes the list of words to be considered as stop words in the given file name
+'''
 def generate_stop_words(tf, df, n, file_name):
-    stop = []
-    term_count = sum(list(tf.values()))
-    doc_count = 1000
+    stop = []                                               # stop words list
+    doc_count = 1000                                        # corpus size
     for term in tf:
-        tcount = tf[term] * n
-        dcount = ((len(df[term]) * n)/doc_count) * 100
-        # print(tcount, dcount)
-        if tcount >= 5500 and dcount >= 65:
+        tcount = tf[term] * n                               # calculate term count for n-grams
+        dcount = ((len(df[term]) * n)/doc_count) * 100      # calculate doc count for n-grams
+        if tcount >= 5500 and dcount >= 65:                 # stopping condition
             stop.append(term)
-    with open(file_name, 'w', encoding='utf-8') as file:
+    with open(file_name, 'w', encoding='utf-8') as file:    # write list to a txt file
         for word in stop:
             file.write("%s\n" %word)
 
 
+'''
+    Given: a term frequency table and a file name
+    Effect: writes table to the given file name with columns term : term-frequency
+'''
 def write_tf_tables(table, file_name):
-    data = sorted(table.items(), key=operator.itemgetter(1), reverse = True)
+    data = sorted(table.items(), key=operator.itemgetter(1), reverse = True)    # sort w.r.t. term frequency
     with open(file_name, 'w', encoding='utf-8') as file:
         for k, v in data:
             file.write("{} : {}\n".format(k, v))
 
+
+'''
+    Given: a document frequency table and a file name
+    Effect: writes table to the given file name with columns term : doc-IDs : document-frequency
+'''
 def write_df_tables(table, file_name):
-    data = sorted(table.items(), key=operator.itemgetter(0), reverse = False)
+    data = sorted(table.items(), key=operator.itemgetter(0), reverse = False)   # sort w.r.t term lexicographically
     with open(file_name, 'w', encoding='utf-8') as file:
         for k, v in data:
             file.write("{} : {} : {}\n".format(k, v, len(v)))
 
+
+'''
+    Given: a table and a file name
+    Effect: writes table to the given file name in json format
+'''
 def write_to_json(table, filename):
     writer = open(filename, 'w')
     json.dump(table, writer)
