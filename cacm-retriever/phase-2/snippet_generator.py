ead18ed4b09a3938373b97c76544083dfdfdfddd
sha5hw4t <shashwat_shetty@hotmail.com>
2018-05-01 20:11:09 -0400

    added comments to snippet generator

--- cacm-retriever/phase-2/snippet_generator.py
+++ cacm-retriever/phase-2/snippet_generator.py
@@ -2,27 +2,36 @@ import json
 import re
 import operator
 
-queries = {}
-stop_list = []
-top_100 = {}
-unigram_tf_table = {}
+# global variables
+queries = {}            # dictionary of queries
+stop_list = []          # list of stop words
+top_100 = {}            # dictionary of the top 100 results
+unigram_tf_table = {}   # unigram term frequency table
+
+# location where Corpus is stored
 CORPUS_LOCATION = "D:/Codes/Practise Code/information-retrieval-systems/cacm-retriever/cleaned-cacm-corpus/"
 
+
 '''
-    Given: a json filename and a dictionary
-    Effect: populates the dictionary with the data in the json file
+    Given: a json filename
+    Returns: a dictionary with the data in the json file
 '''
 def read_from_json(file_name):
     reader = open(file_name, 'r')
-    table = json.load(reader)
+    table = json.load(reader)       # read from the json file
     reader.close()
     return table
 
 
+'''
+    Given: a term frequency dictionary
+    Effect: populates the stop words list as the top 10 most frequent words
+'''
 def get_stop_words(table):
     tf_list = sorted(table.items(), key=operator.itemgetter(1), reverse=True)
     for k, v in tf_list[:11]:
         stop_list.append(k)
+
 
 '''
     Given: a text filename and a dictionary
@@ -30,15 +39,16 @@ def get_stop_words(table):
 '''
 def read_top_100(filename, table):
     file = open(filename, 'r+')
-    lines = [l.strip() for l in file.readlines()]
+    lines = [l.strip() for l in file.readlines()]       # strip each line
     for line in lines:
-        terms = line.split(" ")
-        q = terms[0]
-        d = terms[2]
-        if q in table:
-            table[q].append(d)
+        terms = line.split(" ")                         # split using space
+        q = terms[0]                                    # query ID
+        d = terms[2]                                    # doc ID
+        if q in table:                                  # if query ID exists in table
+            table[q].append(d)                          # append to the list of docs
         else:
-            table[q] = [d]
+            table[q] = [d]                              # else create a new list with doc
+
 
 '''
     Given: a list of terms and an integer n
@@ -51,17 +61,23 @@ def get_ngrams(terms, n):
     return ngrams
 
 
+'''
+    Given: a list of queries, document name and an integer n
+    Returns: a snippet string and true if snippet is not empty, else false
+'''
 def check_ngram_presence(query, doc, n):
-    doc_loc = CORPUS_LOCATION + doc + ".txt"
+    doc_loc = CORPUS_LOCATION + doc + ".txt"        # create doc location string
     file = open(doc_loc, "r+")
-    ngram = get_ngrams(query.split(), n)
+    ngram = get_ngrams(query.split(), n)            # get n-grams of query terms
     doc_content = file.read()
-    doc_len = len(doc_content)
-    for gram in ngram:
-        if gram not in stop_list:
+    doc_len = len(doc_content)                      # length of document
+    for gram in ngram:                              # for each gram
+        if gram not in stop_list:                   # if not in stop list
+            # checks the presence of the query term n-gram in the doc
             if doc_content.find(gram) != -1 and bool(re.findall('\\b' + gram + '\\b', doc_content)):
-                gram_index = doc_content.index(gram)
-                snippet_start = max(gram_index - 50, 0)
+                gram_index = doc_content.index(gram)                # first occurrence of the query term n-gram
+                snippet_start = max(gram_index - 50, 0)             # start index of snippet
+
                 # if start index is in middle of the word, get start of the word
                 if snippet_start > 0:
                     while snippet_start != 0:
@@ -69,7 +85,8 @@ def check_ngram_presence(query, doc, n):
                         if prev_char in [" ", "\n"]:
                             break
                         snippet_start -= 1
-                snippet_end = min(gram_index + n + 50, doc_len)
+                snippet_end = min(gram_index + n + 50, doc_len)     # stop index of snippet
+
                 # if end index is in middle of the word, get end of the word
                 if snippet_end < doc_len:
                     while snippet_end != doc_len - 1:
@@ -78,30 +95,41 @@ def check_ngram_presence(query, doc, n):
                             snippet_end += 1
                             break
                         snippet_end += 1
+
+                # highlight the query terms in snippet
                 highlight_term = "<b>{}</b>".format(gram)
                 snippet = doc_content[snippet_start:snippet_end].replace(gram, highlight_term)
                 return snippet, True
     return "", False
 
+
+'''
+    Given: a list of queries and a document name
+    Returns: a snippet string for that document
+'''
 def get_snippet(query, doc):
-    n = 3
+    n = 3               # counter for the n-gram
     while n > 0:
+        # start with trigram down to unigram
         snippet, has_matched = check_ngram_presence(query, doc, n)
-        if has_matched:
+        if has_matched:         # return if snippet exists
             return snippet
-        n -= 1
-    return "No Snippet Found"
+        n -= 1                  # else reduce counter
+    return "No Snippet Found"   # return when no snippet exists for any n-gram
 
 
+'''
+    Effect: writes data to an html file that contains the snippet for every document
+'''
 def write_snippet():
     file = open("snippets.html", "w+")
-    file.write("<!DOCTYPE html>")
+    file.write("<!DOCTYPE html>")                           # doc header
     for id, query in queries.items():
-        file.write("{Query ID: " + id + "} <br />")
-        for doc in top_100[id]:
+        file.write("{Query ID: " + id + "} <br />")         # snippets for the query ID
+        for doc in top_100[id]:                             # snippet generated from the docs
             file.write("{Doc ID: " + doc + "} <br />")
             file.write("{Snippet} <br />")
-            snippet = get_snippet(query, doc)
+            snippet = get_snippet(query, doc)               # snippet generation call
             file.write(snippet + "<br />")
             file.write(" {\Snippet} <br />")
             file.write("{\Doc ID: " + doc + "} <br />")
@@ -115,7 +143,7 @@ def main():
     queries = read_from_json("queries.json")
     unigram_tf_table = read_from_json("unigrams_tf.json")
     get_stop_words(unigram_tf_table)
-    read_top_100("lucene_baseline.txt", top_100)
+    read_top_100("lucene_baseline.txt", top_100)            # generate snippets for lucene
     write_snippet()
 
 
